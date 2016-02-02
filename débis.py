21add6637796835b66865be2244045565f2b4465
Knoppix User <knoppix@georges.khaznadar.fr>
2016-02-02 13:51:41 +0100

    mise a jour du lancée de dé

---
+++ débis.py
@@ -0,0 +1,63 @@
+def LANCER_DE():
+    import random
+    valeur = random.randint(1,6)
+    return valeur
+
+
+ 
+def SOMME1(n):
+    import random
+    somme = 0
+    for k in range(n):
+        somme=somme +random.randint(1,6)
+      
+    return(somme) 
+
+    
+def SOMME2(n):
+    import random
+    somme = 0
+    for k in range(n):
+        somme=somme +random.randint(1,6)
+    return(somme) 
+    #~ 
+a = SOMME1(1)
+b = SOMME2(1)
+    
+for i in range (1):
+	print (a , end=" ")
+	print (b , end=" ")
+	
+def double(a,b) :
+	double = 0
+	if a == b :
+		print (a+b , end=" ")
+		print ("relancée")		
+		double = double+1
+		return (double)
+	else:
+		print (a+b , end="")
+
+c = double(a,b)
+
+if c > 2
+	print ("allez en prison")
+ 
+else :
+	print ("changée de joueur")
+
+	
+
+
+
+	
+
+	
+	
+	
+
+	
+
+ 
+    
+
