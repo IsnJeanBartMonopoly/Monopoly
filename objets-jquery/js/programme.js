3cb24739951ad5681ddf9b7946e2ee585aa08152
Knoppix User <knoppix@georges.khaznadar.fr>
2016-04-19 13:56:06 +0200

    Plateau de jeu

--- objets-jquery/js/programme.js
+++ objets-jquery/js/programme.js
@@ -1,3 +1,13 @@
-$get(http://127.0.0.1:8080/).done(
-Function (lancededé)
-console.log(lancededé)
+function orange(obj){
+	// obj est un objet jQuery
+	// la fonction renvoie un pointeur sur obj
+	obj.addClass("bouton_actif_clique").removeClass("bouton_actif").val("LANCER_DE");
+	return obj;
+}
+
+function bleu(obj){
+	// obj est un objet jQuery
+	// la fonction renvoie un pointeur sur obj
+	obj.addClass("bouton_actif").removeClass("bouton_actif_clique").val("Cliquez ici");
+	return obj;
+}
